ccbcf96e110461ab71ae8e24bb0bb72d39090c9b
Kevin Hall <kevinmhall8@gmail.com>
2020-03-28 21:23:50 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "employees_DB"
+  });
